f73e77f79a26218a76b7031e254ad916de2a32e6
Billions Joel <joelatugonza@gmail.com>
2022-01-21 14:34:06 +0300

    First try

--- bigSum.js
+++ bigSum.js
@@ -1,6 +1,10 @@
 function aVeryBigSum(ar) {
 	// Write your code here
-  
+	let sum = 0;
+	for (let i = 0; i < ar.length; i++) {
+		sum += ar[i];
+	}
+	console.log(sum);
 }
 
 aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
