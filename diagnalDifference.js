8f034c6d3df0ddc6a61485419cfd5254d3aa2a0f
Billions Joel <joelatugonza@gmail.com>
2022-01-21 16:07:31 +0300

    Thought process step 2

--- diagnalDifference.js
+++ diagnalDifference.js
@@ -2,13 +2,16 @@ function diagonalDifference(arr) {
 	// Write your code here
 	//const n = arr.splice(0, 1);
 
-	let row1 = [11, 2, 4];
-	let row2 = [4, 5, 6];
-	let row3 = [10, 8, -12];
+	let row1 = arr.splice(0, 3);
+	let row2 = arr.splice(0, 3);
+	let row3 = arr.splice(0, 3);
 
 	let left = row1[0] + row2[1] + row3[2];
 	let right = row1[2] + row2[1] + row3[0];
 
+	console.log(row1);
+	console.log(row2);
+	console.log(row3);
 	console.log(Math.abs(left - right));
 }
 
