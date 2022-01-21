4998d7894c50c792cde568faebcc80f5643e2b31
Billions Joel <joelatugonza@gmail.com>
2022-01-21 23:52:15 +0300

    Final solution

--- diagnalDifference.js
+++ diagnalDifference.js
@@ -1,17 +1,20 @@
 function diagonalDifference(arr) {
 	// Write your code here
 	const n = arr.length;
-	let leftD,
-		rightD = 0;
+	let leftD = 0;
+	let rightD = 0;
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < n; j++) {
-			leftD = arr[0][0] + arr[1][1] + arr[2][2];
-			rightD = arr[2][0] + arr[1][1] + arr[0][2];
+			if (i === j) {
+				leftD += arr[i][j];
+			}
+			if (i + j === n - 1) {
+				rightD += arr[i][j];
+			}
 		}
 	}
 
 	console.log(Math.abs(leftD - rightD));
-	//console.log(row2[1][1]);
 }
 
 diagonalDifference([
