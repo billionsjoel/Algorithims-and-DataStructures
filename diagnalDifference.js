f32cc4c45d62e62c11d03b11eb5ec7dccc5241b2
Billions Joel <joelatugonza@gmail.com>
2022-01-21 17:16:59 +0300

    Change function call array to original

--- diagnalDifference.js
+++ diagnalDifference.js
@@ -1,6 +1,6 @@
 function diagonalDifference(arr) {
 	// Write your code here
-	//const n = arr.splice(0, 1);
+	const n = arr.splice(0, 1);
 
 	let row1 = arr.splice(0, 3);
 	let row2 = arr.splice(0, 3);
@@ -9,10 +9,11 @@ function diagonalDifference(arr) {
 	let left = row1[0] + row2[1] + row3[2];
 	let right = row1[2] + row2[1] + row3[0];
 
-	console.log(row1);
-	console.log(row2);
-	console.log(row3);
-	console.log(Math.abs(left - right));
+	return Math.abs(left - right);
 }
 
-diagonalDifference([11, 2, 4, 4, 5, 6, 10, 8, -12]);
+diagonalDifference([
+	[11, 2, 4],
+	[4, 5, 6],
+	[10, 8, -12],
+]);
