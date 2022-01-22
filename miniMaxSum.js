df69e755b3dd13c118f44e64b0049d7462029056
Billions Joel <joelatugonza@gmail.com>
2022-01-22 11:04:56 +0300

    Sort Array

--- miniMaxSum.js
+++ miniMaxSum.js
@@ -1,3 +1,11 @@
 function miniMaxSum(arr) {
 	// Write your code here
+	let finArr = [];
+	let sorted = arr.sort();
+	function reduce(a, b) {
+		return a + b;
+	}
+	console.log(sorted);
 }
+
+miniMaxSum([1, 2, 3, 4, 5]);
