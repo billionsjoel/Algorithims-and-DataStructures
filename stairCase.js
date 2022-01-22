6af947a98334c6b1e8d231574a70685f72cbb59d
Billions Joel <joelatugonza@gmail.com>
2022-01-22 08:15:19 +0300

    Final solution format for js

--- stairCase.js
+++ stairCase.js
@@ -3,7 +3,7 @@ function staircase(n) {
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < i; j++) {
 			if (j === 0) {
-				for (let k = 0; k < n - i; k++) console.log(' ');
+				//for (let k = 0; k < n - i; k++) console.log(' ');
 			}
 			console.log('#');
 		}
