db648ca27d6405e5c4164a6bb45339198dc30e77
Billions Joel <joelatugonza@gmail.com>
2022-01-22 08:10:19 +0300

    Final solution

--- stairCase.js
+++ stairCase.js
@@ -1,3 +1,14 @@
 function staircase(n) {
 	// Write your code here
+	for (let i = 1; i < n; i++) {
+		for (let j = 1; j < i; j++) {
+			if (j === 0) {
+				for (let k = 0; k < n - i; i++) console.log(' ');
+			}
+			console.log('#');
+		}
+		console.log('\n');
+	}
 }
+
+staircase(6);
