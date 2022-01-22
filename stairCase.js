69ecb73cd56fcd6bd0e733dfaf043a6d91a5cf43
Billions Joel <joelatugonza@gmail.com>
2022-01-22 08:13:24 +0300

    Final solution loop edit

--- stairCase.js
+++ stairCase.js
@@ -1,9 +1,9 @@
 function staircase(n) {
 	// Write your code here
-	for (let i = 1; i < n; i++) {
-		for (let j = 1; j < i; j++) {
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < i; j++) {
 			if (j === 0) {
-				for (let k = 0; k < n - i; i++) console.log(' ');
+				for (let k = 0; k < n - i; k++) console.log(' ');
 			}
 			console.log('#');
 		}
