acfff541dc2bfc996bafc1aecfd3daa4dbf03e02
Billions Joel <joelatugonza@gmail.com>
2022-01-23 09:23:07 +0300

    Add pseudo code

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -1,3 +1,18 @@
 function birthdayCakeCandles(candles) {
 	// Write your code here
+	let count = 0;
+	let copyArr = candles;
+	//get all candles ?
+	//sort the candles
+	let sorted = candles.sort();
+	console.log(sorted);
+
+	//get the last candle
+	let lastCandle = copyArr.pop();
+	console.log(lastCandle);
+
+	//loop through all candles and count everytime you meet tallest candle
+
+	//return count
 }
+birthdayCakeCandles([3, 2, 1, 3]);
