130bb52cbd8a89239844054dac5c67f5a751ab3d
Billions Joel <joelatugonza@gmail.com>
2022-01-23 10:25:24 +0300

    Improvong my Old solution and now it works

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -1,16 +1,27 @@
 function birthdayCakeCandles(candles) {
 	// Write your code here
-	let max = 0;
-	let counter = 0;
+	let count = 0;
+	let copyArr = candles;
+	//get all candles ?
+	//sort the candles
+	let sorted = copyArr.sort();
+	//console.log(sorted);
 
-	candles.forEach((item) => {
-		if (item > max) {
-			max = item;
-			counter = 1;
-		} else if (item === max) {
-			counter++;
+	//get the last candle
+	let lastCandle = sorted[sorted.length - 1];
+	//console.log(lastCandle);
+
+	//loop through all candles and count everytime you meet tallest candle
+	for (let i = 0; i <= candles.length; i++) {
+		if (candles[i] === lastCandle) {
+			count++;
 		}
-	});
-	console.log(counter);
+		if (candles[i] > lastCandle) {
+			lastCandle = candles[i];
+			count = 1;
+		}
+	}
+
+	console.log(count);
 }
 birthdayCakeCandles([3, 2, 1, 3, 3]);
