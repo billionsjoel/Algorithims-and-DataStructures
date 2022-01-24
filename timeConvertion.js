8165bfc889f48fc46bec0a6708f9e7b987adb197
Billions Joel <joelatugonza@gmail.com>
2022-01-24 09:20:25 +0300

    Add suspecting pseudo cose

--- timeConvertion.js
+++ timeConvertion.js
@@ -1,5 +1,12 @@
 function timeConversion(s) {
 	// Write your code here
+	// convert time into arr
+
+	// check last array value for am or pm
+
+	// if pm leave time as it is else reset time with a switch case to start from 00;
+
+	//return new string;
 
 }
 
