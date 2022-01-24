a33ca93613b53a62a53f5eea4c52cbb6e971d81a
Billions Joel <joelatugonza@gmail.com>
2022-01-24 09:20:46 +0300

    Split and check for last element

--- timeConvertion.js
+++ timeConvertion.js
@@ -1,13 +1,17 @@
 function timeConversion(s) {
 	// Write your code here
+	let S = '';
 	// convert time into arr
+	const time = s.split(':');
 
 	// check last array value for am or pm
+	const lastEl = time[time.length - 1];
 
 	// if pm leave time as it is else reset time with a switch case to start from 00;
 
 	//return new string;
 
+	console.log(lastEl);
 }
 
 timeConversion('07:05:45PM');
