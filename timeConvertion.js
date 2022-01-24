e531a02f3dae6f8e13b8555b179c285c467e69c1
Billions Joel <joelatugonza@gmail.com>
2022-01-24 12:27:39 +0300

    Testing seconds alone

--- timeConvertion.js
+++ timeConvertion.js
@@ -6,7 +6,13 @@ function timeConversion(s) {
 	const newTime = [];
 	// check last array value for am or pm
 	const lastEl = time[time.length - 1];
+	//console.log(lastEl.split(','));
 	const checkLastEl = lastEl.split('');
+	const seconds = checkLastEl
+		.splice(0, Math.ceil(checkLastEl.length / 2))
+		.join('');
+
+	console.log(seconds);
 
 	// if pm leave time as it is else reset time with a switch case to start from 00;
 
@@ -14,22 +20,24 @@ function timeConversion(s) {
 		console.log(s);
 	} else {
 		// reset the hours
-		if (time[0] === 07) {
+		if (time[0] === '07') {
 			time.unshift('19');
+			//console.log(seconds);
+			console.log(time);
 		}
-		if (time[0] === 08) {
+		if (time[0] === '08') {
 			time.unshift('20');
 		}
-		if (time[0] === 09) {
+		if (time[0] === '09') {
 			time.unshift('21');
 		}
-		if (time[0] === 10) {
+		if (time[0] === '10') {
 			time.unshift('22');
 		}
-		if (time[0] === 11) {
+		if (time[0] === '11') {
 			time.unshift('23');
 		}
-		if (time[0] === 12 && checkLastEl.includes('A')) {
+		if (time[0] === '12' && checkLastEl.includes('A')) {
 			time.unshift('00');
 		}
 
@@ -42,4 +50,4 @@ function timeConversion(s) {
 
 //console.log(newTime);
 
-timeConversion('09:00:00PM');
+timeConversion('07:05:45PM');
