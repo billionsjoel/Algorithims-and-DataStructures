b2d6e7f8d9971cc79f511a65ab01a476901b2c66
Billions Joel <joelatugonza@gmail.com>
2022-01-24 09:54:42 +0300

    Check if Am splice and replace

--- timeConvertion.js
+++ timeConvertion.js
@@ -3,16 +3,25 @@ function timeConversion(s) {
 	let S = '';
 	// convert time into arr
 	const time = s.split(':');
-
+	const newTime = [];
 	// check last array value for am or pm
 	const lastEl = time[time.length - 1];
 	const checkLastEl = lastEl.split('');
 
 	// if pm leave time as it is else reset time with a switch case to start from 00;
 
-	//return new string;
-
-	console.log(checkLastEl);
+	if (checkLastEl.includes('P')) {
+		//console.log(s);
+	} else {
+		// reset the hours
+		time.unshift('13');
+		time.splice(1, 1);
+		console.log(time);
+	}
 }
 
-timeConversion('07:05:45PM');
+//return new string;
+
+//console.log(newTime);
+
+timeConversion('07:05:45AM');
