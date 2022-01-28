3ea3fbdc999313193167adcc5226cef706fddc86
Billions Joel <joelatugonza@gmail.com>
2022-01-28 22:36:36 +0300

    Solution passes all tests

--- gradeStudents.js
+++ gradeStudents.js
@@ -1,15 +1,6 @@
 function gradingStudents(grades) {
-	// Write your code here
-	//if grade is less than 40 === fail
-	//if grade - grade5 < 3 grade === grade5
 	let roundedArr = [];
 	grades.forEach((element) => {
-		//console.log(element % 5);
-
-		//if (element < 38) {
-		//	roundedArr.push(element);
-		//}
-
 		if (element % 5 === 3 && element >= 38) {
 			roundedArr.push(element + 2);
 		} else if (element % 5 === 4 && element >= 38) {
@@ -24,4 +15,4 @@ function gradingStudents(grades) {
 	console.log(roundedArr);
 }
 
-gradingStudents([73, 67, 38, 33]);
+gradingStudents([73, 67, 38, 33, 39]);
