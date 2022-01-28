294c4e14856c7e4de33e6152a386579f239bc7ef
Billions Joel <joelatugonza@gmail.com>
2022-01-28 16:29:50 +0300

    Add pseudo code

--- gradeStudents.js
+++ gradeStudents.js
@@ -1,4 +1,19 @@
 function gradingStudents(grades) {
 	// Write your code here
-  
+	//if grade is less than 40 === fail
+	//if grade - grade5 < 3 grade === grade5
+
+	grades.forEach((element) => {
+		if (element < 40) {
+			return;
+		}
+    if (element % 5 < 3)
+    {
+      // 10 - second element of array , if less than 3 add it to array element .
+		}
+	});
+
+	console.log(roundedArr);
 }
+
+gradingStudents([73, 67, 38, 33]);
