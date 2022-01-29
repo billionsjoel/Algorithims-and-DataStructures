9b8093e02dfbd29d7504cfe1fbb26375b1eb9878
Billions Joel <joelatugonza@gmail.com>
2022-01-29 23:04:49 +0300

    Add our variables

--- betweenTwoSets.js
+++ betweenTwoSets.js
@@ -1,5 +1,14 @@
 function getTotalX(a, b) {
 	// Write your code here
+	let increament = 0;
+	let min = 1;
+	let count = 0;
+	let max = 100;
+
+
+
+
+
 }
 
-getTotalX([2, 4], [(16, 32, 96)]);
+getTotalX([2, 4], [16, 32, 96]);
