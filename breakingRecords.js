fd3b3c80f8c57592eec0f55389fdfe1829cced11
Billions Joel <joelatugonza@gmail.com>
2022-01-29 10:38:09 +0300

    Check for high in the array

--- breakingRecords.js
+++ breakingRecords.js
@@ -1,5 +1,19 @@
 function breakingRecords(scores) {
 	// Write your code here
+	let high = 0;
+	let low = 0;
+	let arr = [];
+	scores.forEach((element) => {
+		if (element > scores[0]) {
+			high += 1;
+			scores[0] = element;
+		}
+	});
+
+
+
+
+	console.log(arr);
 }
 
 breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]);
