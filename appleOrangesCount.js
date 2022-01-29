eff940d1798063a96a989b246ef9b4341c011036
Billions Joel <joelatugonza@gmail.com>
2022-01-29 16:26:53 +0300

    Calculating apple and orange distances

--- appleOrangesCount.js
+++ appleOrangesCount.js
@@ -1,3 +1,16 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
 	// Write your code here
+	let appleDistances = [];
+	let orangeDistances = [];
+	apples.forEach((element) => {
+		appleDistances.push(element + a[0]);
+	});
+	oranges.forEach((element) => {
+		orangeDistances.push(element + a[1]);
+	});
+
+	console.log(appleDistances);
+	console.log(orangeDistances);
 }
+
+countApplesAndOranges(7, 11, [5, 15], [3, 2], [-2, 2, 1], [5, -6]);
