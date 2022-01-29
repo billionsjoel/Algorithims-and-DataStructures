f89e5e6542e0a8f7d9e85509b900f822b0e1998e
Billions Joel <joelatugonza@gmail.com>
2022-01-29 18:50:58 +0300

    Check for the modulus if = 0

--- kangaroo.js
+++ kangaroo.js
@@ -3,7 +3,11 @@ function kangaroo(x1, v1, x2, v2) {
 	let distances = x2 - x1;
 	let velocities = v2 - v1;
 
-
+	if (distances % velocities === 0) {
+		console.log('Yes');
+	} else {
+		console.log('NO');
+	}
 }
 
 kangaroo(0, 3, 4, 2);
