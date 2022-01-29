2cc917c9b5eab0b122b39d58dfe22bb63af22259
Billions Joel <joelatugonza@gmail.com>
2022-01-29 16:31:30 +0300

    Check for condition and increment count

--- appleOrangesCount.js
+++ appleOrangesCount.js
@@ -2,6 +2,9 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
 	// Write your code here
 	let appleDistances = [];
 	let orangeDistances = [];
+	let appleCount = 0;
+	let orangeCount = 0;
+
 	apples.forEach((element) => {
 		appleDistances.push(element + a[0]);
 	});
@@ -9,8 +12,17 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
 		orangeDistances.push(element + a[1]);
 	});
 
-	console.log(appleDistances);
-	console.log(orangeDistances);
+	appleDistances.forEach((element) => {
+		if (element >= s && element <= t) {
+			appleCount++;
+		}
+	});
+	orangeDistances.forEach((element) => {
+		if (element >= s && element <= t) {
+			orangeCount++;
+		}
+	});
+
 }
 
 countApplesAndOranges(7, 11, [5, 15], [3, 2], [-2, 2, 1], [5, -6]);
