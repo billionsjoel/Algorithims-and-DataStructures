dd2bc0a71270c28b736c763434940f937070471a
Billions Joel <joelatugonza@gmail.com>
2022-01-30 09:50:11 +0300

    slice first array and splice rest array values

--- subArray.js
+++ subArray.js
@@ -1,5 +1,8 @@
 function birthday(s, d, m) {
 	// Write your code here
+	console.log(s.slice(0, m));
+	console.log(s.splice(m, m));
+	console.log(s.splice(m, m));
 }
 
 birthday([1, 2, 1, 3, 2], 3, 2);
