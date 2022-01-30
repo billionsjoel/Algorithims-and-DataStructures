e5eedcb1f55931932a4f24b9e8278552a8c1de7f
Billions Joel <joelatugonza@gmail.com>
2022-01-30 15:02:00 +0300

    Added condition for only one element in the array

--- subArray.js
+++ subArray.js
@@ -2,7 +2,12 @@ function birthday(s, d, m) {
 	// Write your code here
 	let sum = 0;
 	let count = 0;
-
+	if (s.length <= 1) {
+		sum = s[0];
+		if (sum === d) {
+			count += 1;
+		}
+	}
 
 	for (let i = 0; i < s.length; i++) {
 		sum = s[i] + s[i + 1];
