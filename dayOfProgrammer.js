355f1b5a9e5da56df69f39e3ebd99276ea7ee866
Billions Joel <joelatugonza@gmail.com>
2022-02-26 10:37:34 +0300

    Pseudo code

--- dayOfProgrammer.js
+++ dayOfProgrammer.js
@@ -1,3 +1,11 @@
 function dayOfProgrammer(year) {
-	// Write your code here
+	// check for which calender the year lies
+  // check if it is a leap year
+  // intialise number of days for each month till august.
+  // Calculate the sum of days until the differece againt 256 is less than 30
+  //return the date [the difference of 256 and the prev sum]
+    // month is [the counter for the previous month summations + 1]
+    // the year is already given
+  //return the year in the form dd.mm.yyyy
+
 }
